17a42b1f8eaeaf13772448644df7e23bdb09b75f
angelPrat <angel.prat@estudiantat.upc.edu>
2023-04-29 12:22:21 +0200

    Task #246 ini fetch + start

--- src/views/EditIssue.vue
+++ src/views/EditIssue.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="master">
+    <div v-if="issue" class="master">
         <div class="wrapper">
             <div class="main detail">
                 <!-- issue info container  -->
@@ -10,38 +10,16 @@
                         <div class="detail-title-wrapper">
                             <span classe="issue-text" style="display: inline-flex;">
                                 <div class="detail-ref">
-                                    <span class="issue-ref">#{{ issue.id }}</span>
+                                    <span class="issue-ref">#{{issue.id}}</span>
+                                    <input name="subject" style="margin-top: 3px; font-size: 25px;" class="detail-subject" value=issue.subject>
+                                    
+                                    <button style="background: none;" type="submit"  name="guardar_subject">
+                                        <i style="margin-top: 3px; font-size: 25px;" class="fa fa-floppy-o" aria-hidden="true"></i>
+                                    </button>
                                 </div>
-                                <input name="subject" style="margin-top: 3px; font-size: 25px;" class="detail-subject" value="{{issue.subject}}">
-                                <button style="background: none;" type="submit"  name="guardar_subject">
-                                    <i style="margin-top: 3px; font-size: 25px;" class="fa fa-floppy-o" aria-hidden="true"></i>
-                                </button>
+                                
                             </span>
                         </div>
-                        <div class="detail-project">
-                            <div class="section-name">Issue</div>
-                        </div>
-                        <!--
-                            <div v-if="issue.dataLimit" class="detail-header-line block-desc-container">
-                                <span class="blocked-sign" style="color: white">
-                                    <i class="fa fa-clock-o" aria-hidden="true" style="margin-right: 5px; margin-top: 1px;"></i>
-                                </span>
-                                <span class="block-description" style="margin-left: 5px; color: #e5e5e5;">
-                                    {{issue.dataLimit}}
-                                </span>
-                            </div>
-                            <div v-if="issue.bloquejat" class="detail-header-line block-desc-container">
-                                <span class="blocked-sign" style="color: white">
-                                    <i class="fa fa-lock" aria-hidden="true" style="margin-right: 5px; margin-top: 1px;"></i>
-                                    Blocked
-                                </span>
-                                <span class="block-description" style="margin-left: 5px; color: #e5e5e5;">
-                                        {{issue.motiuBloqueig}}
-                                </span>
-                            </div> 
-                        -->   
-                        <!-- Action buttons-->
-                        
                     </div>
                 </div>
             </div>
@@ -57,10 +35,9 @@
     export default {
         setup() {
             let issue = ref();
-
             
             return {
-                issue
+                issue,
             }
         },
         mounted() {
@@ -73,11 +50,8 @@
 
 
             console.log("urlLocation: ", issueId);
-            let dataIssue;
-            simpleFetch("issues/"+issueId+"/", "GET", "").then((data) => dataIssue = data);
-            issue.value = dataIssue;
-            console.log("dataIssue: ", dataIssue);
-            console.log("issueObject: ", issue.value.id);
+            simpleFetch("issues/"+issueId+"/", "GET", "").then((data) => this.issue = data);
+            console.log("issueObject: ", this.issue);
         }
     }
 
