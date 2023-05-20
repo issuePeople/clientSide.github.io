4a6d46b85598de1aa1e0fe356dd85974636438b0
s-torralba <sol.torralbac02@gmail.com>
2023-05-20 16:11:28 +0200

    Començada pantalla amb info d'usuaris

--- src/views/UsuariInfo.vue
+++ src/views/UsuariInfo.vue
@@ -1,35 +1,75 @@
 <template>
-    <div>
-        user info
-    </div>
+      
 </template>
 
 <script>
     //imports
+    import { ref } from 'vue';
+    import {simpleFetch} from '@/utils/utils';
     
     export default {
         name: "UsuariInfo",
-        /*
-        components: {
-            exempleComponent
-        },
-        Si es necesari l'estructura es aixi
-        props: {
-            title: String
-        },
-        setup(props) {
-        */
         setup() {
 
-            return {
+            //Per obtenir la url
+            let url = window.location.href;
+            //Separar la url per '/'
+            let directories = url.split("/");
+            let userId = ref(directories[(directories.length - 1)]);
+
+            let allUsers = ref([]);
+            simpleFetch("usuaris/", "GET", "").then((data) => allUsers.value = data);
+            console.log("all Users: ", allUsers.value);
+
+            async function canviar_timeline() {
 
             }
+
+            async function canviar_watched() {
+                
+            }
+
+            async function canviar_token() {
+                
+            }
+
+            async function canviar_usuari() {
+                
+            }
+
+            return {
+                allUsers,
+            }
+        },
+        
+        //hacer funciones antes de que se pinte todo
+        mounted() {
+            //Per obtenir la url
+            let url = window.location.href;
+            //Separar la url per '/'
+            let directories = url.split("/");
+            let userId = directories[(directories.length - 1)];
+            
+            //Obtengo todos los usuarios para ponerlos en el apartado "Your Team"
+            let allUsers = ref([]);
+            simpleFetch("usuaris/", "GET", "").then((data) => allUsers.value = data);
+            console.log("all Users: ", allUsers.value);
+
+            //Obtengo el usuario loggeado
+            let idUser = ref([]);
+            let userLogged = ref([]);
+            simpleFetch("usuaris/"+idUser.value+"/", "GET", "").then((data) => userLogged.value = data);
+
         }
     }
 </script>
 
 <style scoped>
-    
+    .my-tag {
+    display: flex;
+    justify-content: space-between;
+    margin-right: 5px;
+}
 </style>
 
 
