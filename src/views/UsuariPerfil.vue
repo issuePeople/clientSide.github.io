9431a9b5ab978d16c120eff52ee6f253d18b2a9f
s-torralba <sol.torralbac02@gmail.com>
2023-05-27 15:18:55 +0200

    Modificacions

--- src/views/UsuariPerfil.vue
+++ src/views/UsuariPerfil.vue
@@ -1,30 +1,50 @@
+
 <template>
-  <RouterLink to="/usuari/info">Perfil</RouterLink>
-  <div class="about">
-    <h1>This is an about user perfil page</h1>
+  <div class="master" style="width: 100%; display: grid; place-content: center;">
+    <section class="main user-profile">
+        <header>
+            <header>
+                <h1 style="color: #1e96b1;">Configuració d'usuari</h1>
+            </header>
+        </header>
+    </section>
   </div>
 </template>
 
 <script>
   //imports
+  import { ref } from 'vue';
+  import {simpleFetch} from '@/utils/utils'; 
+    
 
   export default {
     name: "UsuariPerfil",
-    /*
-    components: {
-      exempleComponent
-    },
-    Si es necesari l'estructura es aixi
-    props: {
-      title: String
-    },
-    setup(props) {
-    */
     setup() {
+      
+      //Per obtenir la url
+      let url = window.location.href;
+      console.log("Url: ",url);
+      //Separar la url per '/'
+      let directories = url.split("/");
+      let idUser = ref(directories[(directories.length - 1)]);
+      
+      let userLogged = ref([]);
 
       return {
-
+        userLogged
       }
+    },
+
+    mounted() {
+      //Per obtenir la url
+      let url = window.location.href;
+      //Separar la url per '/'
+      let directories = url.split("/");
+      let userId = directories[(directories.length - 1)];
+      
+      //Obtengo el usuario loggeado
+      simpleFetch("usuaris/"+1+"/", "GET", "").then((data) => this.userLogged = data);
+      console.log("Logged user: ", this.userLogged);
     }
   }
 
