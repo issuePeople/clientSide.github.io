2c587d7674042dfc072f13970e61503dcb0659d5
angelPrat <angel.prat@estudiantat.upc.edu>
2023-05-28 17:32:26 +0200

    Updates

---
+++ assets/Login-526f9203.js
@@ -0,0 +1,1 @@
+import{o as e,c as o}from"./index-9fe29b53.js";const a={__name:"Login",setup(n){return(t,c)=>(e(),o("div",null," Login "))}};export{a as default};
---
+++ assets/SingUp-87439d68.js
@@ -0,0 +1,1 @@
+import{o as e,c as n}from"./index-9fe29b53.js";const a={__name:"SingUp",setup(t){return(c,o)=>(e(),n("div",null," SingUp "))}};export{a as default};
