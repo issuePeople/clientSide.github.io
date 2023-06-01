39cd8c4fceadfa691826370c382a8df446916bf6
angelPrat <angel.prat@estudiantat.upc.edu>
2023-06-01 18:12:44 +0200

    Updates

--- assets/SingUp-750ede89.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as n}from"./index-e3c4a6b4.js";const a={__name:"SingUp",setup(t){return(c,o)=>(e(),n("div",null," SingUp "))}};export{a as default};
---
+++ assets/SingUp-e4bc3d8c.js
@@ -0,0 +1,1 @@
+import{o as e,c as n}from"./index-8b1e70ea.js";const a={__name:"SingUp",setup(t){return(c,o)=>(e(),n("div",null," SingUp "))}};export{a as default};
